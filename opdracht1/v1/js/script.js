ec53adf795bcf469733570f5da23657e966673d5
Rob de Groot <groot_rob@live.nl>
2019-02-27 09:55:53 +0100

    Versie 1

---
+++ opdracht1/v1/js/script.js
@@ -0,0 +1,54 @@
+(function () {
+    filterContent();
+})();
+
+function filterContent() {
+    //    var color = this.getAttribute("id");
+    var kleurKeuze = document.querySelectorAll("input");
+    var blauw = document.querySelectorAll(".blauw");
+    var rood = document.querySelectorAll(".rood");
+    var groen = document.querySelectorAll(".groen");
+    var geel = document.querySelectorAll(".geel");
+    var roze = document.querySelectorAll(".roze");
+    var oranje = document.querySelectorAll(".oranje");
+    var wit = document.querySelectorAll(".wit");
+    var zwart = document.querySelectorAll(".zwart");
+
+
+    if () {
+        //        blauw.classList.add("show");
+        //        kleurKeuze.classList.remove("default");
+        console.log("Hij doet het nu ook");
+
+    } else if (user == "B") {
+        console.log("kechie");
+
+    } else if (user == "C") {
+
+    }
+}
+
+//
+//$(document).ready(function () {
+//
+//    $("checkbox").click(function () {
+//        var value = $(this).attr('data-filter');
+//
+//        if (value == "all") {
+//            //$('.filter').removeClass('hidden');
+//            $('.filter').show('1000');
+//        } else {
+//            //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+//            $(".filter").not('.' + value).hide('3000');
+//            $('.filter').filter('.' + value).show('3000');
+//
+//        }
+//    });
+//
+//    if ($(".filter-check").removeClass("active")) {
+//        $(this).removeClass("active");
+//    }
+//    $(this).addClass("active");
+//
+//});
