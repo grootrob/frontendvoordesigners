6a2d419c3a4f3e96ba2f0664268a75ca6b1fabac
Rob de Groot <groot_rob@live.nl>
2019-02-27 10:01:17 +0100

    versie 1.1

--- opdracht1/v1/js/script.js
+++ opdracht1/v1/js/script.js
@@ -1,3 +1,7 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
 (function () {
     filterContent();
 })();
@@ -20,35 +24,10 @@ function filterContent() {
         //        kleurKeuze.classList.remove("default");
         console.log("Hij doet het nu ook");
 
-    } else if (user == "B") {
-        console.log("kechie");
-
-    } else if (user == "C") {
-
+        //    } else if (user == "B") {
+        //        console.log("hoi");
+        //
+        //    } else if (user == "C") {
+        //
+        //    }
     }
-}
-
-//
-//$(document).ready(function () {
-//
-//    $("checkbox").click(function () {
-//        var value = $(this).attr('data-filter');
-//
-//        if (value == "all") {
-//            //$('.filter').removeClass('hidden');
-//            $('.filter').show('1000');
-//        } else {
-//            //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-//            $(".filter").not('.' + value).hide('3000');
-//            $('.filter').filter('.' + value).show('3000');
-//
-//        }
-//    });
-//
-//    if ($(".filter-check").removeClass("active")) {
-//        $(this).removeClass("active");
-//    }
-//    $(this).addClass("active");
-//
-//});
