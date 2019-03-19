4fdac85d5c0d002c8b91eb13fb3d60dd789654ad
Rob de Groot <groot_rob@live.nl>
2019-03-19 18:01:54 +0100

    mrt19

---
+++ opdracht2/v2/js/all.js
@@ -0,0 +1,61 @@
+var buttonVliegen = document.querySelector(".buttonVliegen");
+var buttonDood = document.querySelector(".buttonDood");
+var buttonLaser = document.querySelector(".buttonLaser");
+var vogel = document.querySelector(".vogel");
+var oogDood = document.querySelector(".oogDood");
+var oogDood2 = document.querySelector(".oogDood2");
+var oogLevend = document.querySelector(".oog");
+var refresh = document.querySelector(".reloadbar");
+var laserstraal = document.querySelector(".lasers");
+
+buttonVliegen.addEventListener("click", vliegen);
+buttonDood.addEventListener("click", dood);
+buttonLaser.addEventListener("click", lasers);
+
+refresh.addEventListener("click", refreshen);
+
+function refreshen() {
+    location.reload();
+}
+
+function vliegen() {
+    vogel.classList.add("vliegen");
+    console.log("flapflapflap");
+    refresh.style.display = "block";
+}
+
+function lasers() {
+    console.log("LASERRSSSS!!!!");
+    laserstraal.style.display = "block";
+    refresh.style.display = "block";
+}
+
+function dood() {
+    vogel.classList.add("dood");
+    oogLevend.style.display = "none";
+    oogDood.style.display = "block";
+    oogDood2.style.display = "block";
+    refresh.style.display = "block";
+    console.log("He ded");
+}
+
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 86) {
+        vliegen();
+    }
+    if (event.keyCode === 68) {
+        dood();
+    }
+
+    if (event.keyCode === 76) {
+        lasers();
+    }
+
+    if (event.keyCode === 82) {
+        refreshen();
+    }
+
+    if (event.keyCode === 68) {
+        dood();
+    }
+});
